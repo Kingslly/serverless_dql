8434a5f4b41f4e4c5d6b9b058d5746d4060848c0
Kingslly RA <102270555+Kingslly@users.noreply.github.com>
2023-06-15 15:41:25 +0530

    producer.py

--- serverless_dql/handler/producer.py
+++ serverless_dql/handler/producer.py
@@ -1,9 +1,9 @@
-import boto3
+import json
 
 def producer(event, context):
     # Logic to push messages to the queue
     # You can use the AWS SDK to send messages to the queue
     return {
         "statusCode": 200,
-        "body": boto3.dumps({"message": "Messages pushed to the queue"})
+        "body": json.dumps({"message": "Messages pushed to the queue"})
     }
